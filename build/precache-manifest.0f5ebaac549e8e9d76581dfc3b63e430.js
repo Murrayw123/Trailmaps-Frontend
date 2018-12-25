5cdea5b2df9f2294f346bd4ffded1cc96bf1e31e
Murray Bishop <murraybishop@MacBook-Pro-2.local>
2018-12-25 21:00:26 +0800

    new build

---
+++ build/precache-manifest.0f5ebaac549e8e9d76581dfc3b63e430.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb071ea368023e88674116477a715b10",
+    "url": "/static/media/hiking.eb071ea3.svg"
+  },
+  {
+    "revision": "17e96ae9db6a15a58ec8",
+    "url": "/static/css/main.313ffb97.chunk.css"
+  },
+  {
+    "revision": "9aa4630f6f2534c0b7a143599a1741f4",
+    "url": "/static/media/background-landingpage.9aa4630f.jpg"
+  },
+  {
+    "revision": "a7fbba8168524970c535",
+    "url": "/static/js/1.a7fbba81.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b3a6e157de24fd5ef6ef45273602bee",
+    "url": "/static/media/start.2b3a6e15.svg"
+  },
+  {
+    "revision": "b336a83d3b72ff5f17b3067fb4d6bd24",
+    "url": "/static/media/finish.b336a83d.svg"
+  },
+  {
+    "revision": "e87899aec4a72f04c2466f49e6d40f8f",
+    "url": "/static/media/business.e87899ae.svg"
+  },
+  {
+    "revision": "971f6a4f0231dde44c2124eac153615d",
+    "url": "/static/media/bicycle.971f6a4f.svg"
+  },
+  {
+    "revision": "17e96ae9db6a15a58ec8",
+    "url": "/static/js/main.17e96ae9.chunk.js"
+  },
+  {
+    "revision": "051bcc3a18878dea64fc0707a008f71d",
+    "url": "/static/media/tent.051bcc3a.svg"
+  },
+  {
+    "revision": "4687401e684a511ab972e4a38f772b4d",
+    "url": "/static/media/hut.4687401e.svg"
+  },
+  {
+    "revision": "5c095e44158e2f28587bf52461017b77",
+    "url": "/static/media/town.5c095e44.svg"
+  },
+  {
+    "revision": "1006b3287ce1eda20ad2212edb386a0b",
+    "url": "/static/media/water.1006b328.svg"
+  },
+  {
+    "revision": "cb8356bfad49693d1ee73df1b2c8bce6",
+    "url": "/static/media/attraction.cb8356bf.svg"
+  },
+  {
+    "revision": "10d973751639043a3e6b09fd39342823",
+    "url": "/static/media/vehicle.10d97375.svg"
+  },
+  {
+    "revision": "34e4a38845e3448425a9f2bdbcb9e0b0",
+    "url": "/static/media/selected.34e4a388.svg"
+  },
+  {
+    "revision": "a7fbba8168524970c535",
+    "url": "/static/css/1.bd0f0edc.chunk.css"
+  },
+  {
+    "revision": "26487de2bfacb7c23894c49baf0463df",
+    "url": "/index.html"
+  }
+];
