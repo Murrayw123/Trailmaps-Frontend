ea0aa1dcdc2d1c6b27899a8e2ef80e3d5f63e193
Murray Bishop <murraybishop@MacBook-Pro-2.local>
2019-01-08 08:12:04 +0800

    small changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.883b36a4a7b5ae984d5830f14741a138.js"
+  "/precache-manifest.1b3a696ae6a7d891f151c043598467cd.js"
 );
 
 workbox.clientsClaim();
