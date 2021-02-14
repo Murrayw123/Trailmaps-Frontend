07e538df8ee872ae3e27f24661da9d54a245cd3d
Murray <murraycwatts@gmail.com>
2021-02-14 12:15:03 +0800

    go to point

--- src/components/MenuComponents/PointOnMapComponent.tsx
+++ src/components/MenuComponents/PointOnMapComponent.tsx
@@ -1,27 +1,38 @@
 import { Divider } from "antd";
 import React from "react";
 import DistanceCalculator from "components/DistanceCalculator";
+import { changeFocusPoint } from "redux/actions";
+import { Dispatch } from "redux";
+import { Marker } from "Interfaces/Marker";
+import { LocateLatLong } from "components/MenuComponents/LocateLatLong";
 
 interface Props {
-  poiMarkers: unknown;
+  poiMarkers: Array<Marker>;
   dataSelect: unknown;
   focusMarker: unknown;
+  dispatch: Dispatch;
 }
 
 export class PointOnMap extends React.Component<Props, never> {
-  render() {
+  onLatLngChanged = (lat: number, long: number): void => {
+    this.props.dispatch(changeFocusPoint([lat, long]));
+  };
+
+  render(): JSX.Element {
     const { poiMarkers, focusMarker, dataSelect } = this.props;
 
     return (
       <>
         <DistanceCalculator
-          placeHolder={"Point on map"}
+          placeHolder={"Find marker on map"}
           dataSource={poiMarkers}
           type={"locate"}
           onSelect={dataSelect}
           value={focusMarker}
           markerType={"distance"}
         />
+        <Divider style={{ marginTop: 11, marginBottom: 11 }} />
+        <LocateLatLong onChange={this.onLatLngChanged} />
       </>
     );
   }
