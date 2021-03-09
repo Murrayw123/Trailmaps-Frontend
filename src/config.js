d108d7f85e166c5d3c1a41d901c3c7c810df20b7
Murray <murraycwatts@gmail.com>
2021-03-10 00:28:41 +0800

    cypress run

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 import { isDevEnvironment, isTestEnvironment } from "./utilities";
 
 const environment = () => {
-  if (isDevEnvironment()) {
-    return process.env.REACT_APP_URLPREFIX;
-  }
   if (isTestEnvironment()) {
     // being intercepted anyway
     return "http://localhost:8000";
   }
-  return ""
+  if (isDevEnvironment()) {
+    return process.env.REACT_APP_URLPREFIX;
+  }
+  return "";
 };
 
 export const URLPREFIX = environment();
