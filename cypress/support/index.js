df26d816ae77439d15c52e8a8d67df9a65dbb014
Murray <murraycwatts@gmail.com>
2021-03-09 21:04:57 +0800

    cypress run

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -5,21 +5,15 @@ beforeEach(() => {
   cy.intercept("GET", "/api/map_preview", { fixture: "map_preview.json" });
   cy.intercept("GET", "/api/spotuserswithlocation", []);
 
-  cy.intercept("GET", "/api/maps/mundabiddi", { fixture: "mundabiddi.json" });
-  cy.intercept("GET", "/api/markers*", { fixture: "markers.json" });
-  cy.intercept("POST", "/api/submit_marker/*", {});
-  cy.intercept("GET", "/api/map_preview", { fixture: "map_preview.json" });
-  cy.intercept("GET", "/api/spotuserswithlocation", []);
-
-  cy.intercept("GET", "https://trailmaps.site/api/maps/mundabiddi", {
+  cy.intercept("GET", "http://localhost:3000/api/maps/mundabiddi", {
     fixture: "mundabiddi.json",
   });
-  cy.intercept("GET", "https://trailmaps.site/api/markers*", {
+  cy.intercept("GET", "http://localhost:3000/api/markers*", {
     fixture: "markers.json",
   });
-  cy.intercept("POST", "https://trailmaps.site/api/submit_marker/*", {});
-  cy.intercept("GET", "https://trailmaps.site/api/map_preview", {
+  cy.intercept("POST", "http://localhost:3000/api/submit_marker/*", {});
+  cy.intercept("GET", "http://localhost:3000/api/map_preview", {
     fixture: "map_preview.json",
   });
-  cy.intercept("GET", "https://trailmaps.site/api/spotuserswithlocation", []);
+  cy.intercept("GET", "http://localhost:3000/api/spotuserswithlocation", []);
 });
