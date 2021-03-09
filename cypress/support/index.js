75da0baa727b345bb06f9652a141e80a25001606
Murray <murraycwatts@gmail.com>
2021-03-09 21:46:47 +0800

    cypress run

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -20,4 +20,16 @@ beforeEach(() => {
     "https://trailmaps.site:8000/api/spotuserswithlocation",
     []
   );
+
+  cy.intercept("GET", "http://localhost:8000/api/maps/mundabiddi", {
+    fixture: "mundabiddi.json",
+  });
+  cy.intercept("GET", "http://localhost:8000/api/markers*", {
+    fixture: "markers.json",
+  });
+  cy.intercept("POST", "http://localhost:8000/api/submit_marker/*", {});
+  cy.intercept("GET", "http://localhost:8000/api/map_preview", {
+    fixture: "map_preview.json",
+  });
+  cy.intercept("GET", "http://localhost:8000/api/spotuserswithlocation", []);
 });
