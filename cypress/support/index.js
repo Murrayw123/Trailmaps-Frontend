916016f716edef51f1ec2c46bcaff830a1cdaa7b
Murray <murraycwatts@gmail.com>
2021-04-07 07:38:21 +0800

    fixing the fixtures

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -4,4 +4,5 @@ beforeEach(() => {
   cy.intercept("POST", "/api/submit_marker/*", {});
   cy.intercept("GET", "/api/map_preview", { fixture: "map_preview.json" });
   cy.intercept("GET", "/api/spot_users_with_location", []);
+  cy.intercept("GET", "https://trailmaps-geojson.s3-ap-southeast-2.amazonaws.com/mundabiddi.geojson", { fixture: "mundabiddi_track.json" });
 });
